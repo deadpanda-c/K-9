f0bd9c96f0d9f8239dcaa298ec147dac65bdc68c
Clément Lagier <clement.lagier@epitech.eu>
2025-11-14 11:52:35 +0100

    [ADD] Added the script for the sensor

---
+++ radar/main.py
@@ -0,0 +1,32 @@
+import RPI.GPIO as GPIO
+import time
+
+if __name__ == "__main__":
+  GPIO.setmode(GPIO.BCM)
+
+  TRIG = 23
+  ECHO = 24
+
+  print("Distance measurement in progress...")
+
+  GPIO.setup(TRIG, GPIO.OUT)
+  GPIO.setup(ECHO, GPIO.IN)
+
+  GPIO.output(TRIG, False)
+  print("Waiting for sensor to settle...")
+  time.sleep(2)
+
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
+
+  while GPIO.input(ECHO) == 0:
+    pulse_start = time.time()
+    while GPIO.input(ECHO) == 1:
+      pulse_end = time.time()
+      pulse_duration = pulse_end - pulse_start
+      distance = pulse_duration * 17150
+      distance = round(distance, 2)
+      print("Distance: {} cm".format(distance))
+
+  GPIO.cleanup()

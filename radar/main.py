8a485ba132216cc19955d14810bee7af1696f18a
Clément Lagier <clement.lagier@epitech.eu>
2025-11-14 14:13:01 +0100

    [UPDATE] Put the calc snippet in a loop

--- radar/main.py
+++ radar/main.py
@@ -1,6 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
+def distance():
+  while GPIO.input(ECHO) == 0:
+    pass
+  pulse_start = time.time()
+  while GPIO.input(ECHO) == 1:
+    pass
+  pulse_end = time.time()
+  pulse_duration = pulse_end - pulse_start
+  distance = pulse_duration * 17150
+  distance = round(distance, 2)
+  print("Distance: {} cm".format(distance))
+
 if __name__ == "__main__":
   GPIO.setmode(GPIO.BCM)
 
@@ -20,15 +32,8 @@ if __name__ == "__main__":
   time.sleep(0.00001)
   GPIO.output(TRIG, False)
 
-  while GPIO.input(ECHO) == 0:
-    pass
-  pulse_start = time.time()
-  while GPIO.input(ECHO) == 1:
-    pass
-  pulse_end = time.time()
-  pulse_duration = pulse_end - pulse_start
-  distance = pulse_duration * 17150
-  distance = round(distance, 2)
-  print("Distance: {} cm".format(distance))
+  while True:
+    distance()
+
 
   GPIO.cleanup()

2a53c1d352be2349170837df58234b6aef82b56d
Clément Lagier <clement.lagier@epitech.eu>
2025-11-14 14:15:06 +0100

    [UPDATE] Added a try/except

--- radar/main.py
+++ radar/main.py
@@ -28,12 +28,14 @@ if __name__ == "__main__":
   print("Waiting for sensor to settle...")
   time.sleep(2)
 
-  GPIO.output(TRIG, True)
-  time.sleep(0.00001)
-  GPIO.output(TRIG, False)
+  try:
+    while True:
+      GPIO.output(TRIG, True)
+      time.sleep(0.00001)
+      GPIO.output(TRIG, False)
+      distance()
+      time.sleep(0.5)
+  except KeyboardInterrupt:
+    GPIO.cleanup()
 
-  while True:
-    distance()
 
-
-  GPIO.cleanup()
